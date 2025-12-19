efb53b9dc247b3afe2ec1bd7752757b72d6a1842
alexander.orlov <Alexander.Orlov@huur-us.com>
2025-12-19 03:55:21 -0500

    mobile scan fixed

---
+++ client/src/pages/MobileScan.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ScanLicenseNative from './ScanLicenseNative';
+
+// MobileScan simply wraps the native scanner for the mobile route
+const MobileScan = () => {
+  return <ScanLicenseNative />;
+};
+
+export default MobileScan;
+
